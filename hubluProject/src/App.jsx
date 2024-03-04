11c977a75efc40911f2d9688a3f25b13b2de87db
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-03-04 19:14:25 +0600

    first commit

---
+++ hubluProject/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Home from "./Pages/Home"
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+        {/* <h1>Hello World</h1> */}
+        <Home/>
+        <About/>
+        <Contact/>
+    </div>
+  )
+}
+
+export default App
---
+++ hubluProject/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+var Name = "Masud Parvez";
+function Home() {
+    return (
+        <div>
+            <h1>Hello Parvez</h1>
+            <h1>{Name}</h1>
+        </div>
+    )
+}
+
+export default Home;
